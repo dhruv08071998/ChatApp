ba4cd667ca0a754a0a7b3b6cabb8013df5255875
Dhruv Upadhyay <dhruv@quarkstudios.com>
2020-04-19 09:33:03 +0530

    Initial Commit

---
+++ SocketChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocketChatApp
+//
+//  Created by Dhruv Upadhyay on 19/04/20.
+//  Copyright © 2020 Dhruv Upadhyay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
